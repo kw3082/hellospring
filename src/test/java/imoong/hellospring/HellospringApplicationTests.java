95a85d377b9c4554e55042151e1200c53b0b6b21
kw3082 <kw3082>
2025-11-07 17:58:21 +0900

    initialize

---
+++ src/test/java/imoong/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package imoong.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
